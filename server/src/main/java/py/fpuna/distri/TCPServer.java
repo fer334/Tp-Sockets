fe5490ab61922ca7c6180dedb4c44ee954538864
fer <luis.fer334@live.com>
2020-03-24 02:59:15 -0400

    Se agrego los dos proyectos que pide el enunciado del TP

---
+++ server/src/main/java/py/fpuna/distri/TCPServer.java
@@ -0,0 +1,27 @@
+package py.fpuna.distri;
+
+import java.net.*;
+import java.io.*;
+
+public class TCPServer {
+
+    public static void main(String[] args) throws IOException {
+        //String inputLine, outputLine;
+        ServerSocket serverSocket = null;
+        //Socket clientSocket = null;
+        boolean listening = true;
+        try {
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.err.println("No se puede abrir el puerto: 4444.");
+            System.exit(1);
+        }
+        System.out.println("Puerto abierto: 4444.");
+
+        while (listening) {
+            new TCPServerHilo(serverSocket.accept()).start();
+        }
+
+        serverSocket.close();
+    }
+}
